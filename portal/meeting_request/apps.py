d3f3b1b46ef87c84fb1f000d89879265780a18f6
Ali Sajadian <A.Sajadian@ASFT.CO>
2021-04-28 09:11:45 +0430

    'pb_add_app'

---
+++ portal/meeting_request/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class MeetingRequestConfig(AppConfig):
+    name = 'meeting_request'
---
+++ portal/resturaunt/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ResturauntConfig(AppConfig):
+    name = 'resturaunt'
